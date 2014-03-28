b925fe2c09cf59a16230efa73b72976fe251f1e4
Yatender Kumar <yatin.smile@gmail.com>
2014-03-28 18:34:17 +0530

    wp-config commit

--- eshopbox/wp-config.php
+++ eshopbox/wp-config.php
@@ -13,7 +13,7 @@
  *
  * @package WordPress
  */
-
+echo "lubu"."wp-config";
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'ekmatra');
